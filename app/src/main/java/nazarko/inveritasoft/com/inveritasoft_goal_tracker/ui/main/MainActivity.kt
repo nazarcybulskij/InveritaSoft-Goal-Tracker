137258708860f016ebadd9583059e2f28a498e63
nazar.zhybulski@gmail.com <nazar.zhybulski@gmail.com>
2018-02-09 17:32:35 +0200

    UI  changed

--- app/src/main/java/nazarko/inveritasoft/com/inveritasoft_goal_tracker/ui/main/MainActivity.kt
+++ app/src/main/java/nazarko/inveritasoft/com/inveritasoft_goal_tracker/ui/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package nazarko.inveritasoft.com.inveritasoft_goal_tracker.ui.main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.CalendarMode
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,8 +25,24 @@ class MainActivity : HabitsActivity(),OnDateSelectedListener {
     }
 
     private fun initviews() {
+        var calendar = Calendar.getInstance();
+        calendarView.currentDate =  CalendarDay.from(calendar.time);
         calendarView.setOnDateChangedListener(this)
         calendarView.addDecorators(HighlightWeekendsDecorator(),SelectedDecorator(this))
+        weekButton.setOnClickListener( object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                calendarView.state().edit()
+                        .setCalendarDisplayMode(CalendarMode.WEEKS)
+                        .commit()
+            }
+        });
+        monthButton.setOnClickListener( object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                calendarView.state().edit()
+                        .setCalendarDisplayMode(CalendarMode.MONTHS)
+                        .commit()
+            }
+        });
     }
 
     override fun onDateSelected(widget: MaterialCalendarView, date: CalendarDay, selected: Boolean) {
