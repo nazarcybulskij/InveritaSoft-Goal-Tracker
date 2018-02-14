8a1d4c6ddf603b58ffacd4c9e276098d839a4f4d
nazar.zhybulski@gmail.com <nazar.zhybulski@gmail.com>
2018-02-14 19:27:27 +0200

    Note Dialog UI

--- app/src/main/java/nazarko/inveritasoft/com/inveritasoft_goal_tracker/ui/view/NoteDialog.kt
+++ app/src/main/java/nazarko/inveritasoft/com/inveritasoft_goal_tracker/ui/view/NoteDialog.kt
@@ -5,26 +5,27 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.app.DialogFragment
 import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import nazarko.inveritasoft.com.inveritasoft_goal_tracker.R
-import android.R.attr.x
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.*
-import android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE
 import android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
-import android.widget.LinearLayout
+import android.widget.TextView
+import java.text.SimpleDateFormat
 
 
 /**
  * Created by nazarko on 14.02.18.
  */
-class NoteDialog : DialogFragment() {
+class NoteDialog : DialogFragment(){
 
 
     interface NodeDialogListener{
-
+        fun  cancelComment()
+        fun deleteComment()
+        fun setComment()
     }
 
     companion object {
@@ -52,7 +53,24 @@ class NoteDialog : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val view = activity.layoutInflater.inflate(R.layout.dialog_note, null)
-        return AlertDialog.Builder(activity)
+        var date =  arguments.getParcelable(BUNDLE_DIALOG_DATE) as CalendarDay
+        var dateFormatDayFormatter = SimpleDateFormat("dd MMM yyyy")
+
+        view.findViewById<TextView>(R.id.dateSubtitleText).text =resources.getString(R.string.write_note
+        )+ " ${dateFormatDayFormatter.format(date.date)}.";
+        view.findViewById<View>(R.id.cancel).setOnClickListener(View.OnClickListener {
+            listener?.cancelComment()
+            dismiss()
+        })
+        view.findViewById<View>(R.id.set).setOnClickListener(View.OnClickListener {
+            listener?.setComment()
+            dismiss()
+        })
+        view.findViewById<View>(R.id.delete).setOnClickListener(View.OnClickListener {
+            listener?.deleteComment()
+            dismiss()
+        })
+        return AlertDialog.Builder(activity,R.style.NoteDialogThema)
                 .setView(view)
                 .create()
                 .apply {
@@ -65,5 +83,7 @@ class NoteDialog : DialogFragment() {
                     window.attributes = params
                 }
     }
+
+
 
 }
